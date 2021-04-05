5cfa61452c210ae3529eb991442d854356b43200
Bridget <78001641+ANTWIWBA@users.noreply.github.com>
2021-04-04 21:23:42 -0400

    Add files via upload

---
+++ Regression_Line_1.R
@@ -0,0 +1,26 @@
+library(readr)
+library(stargazer)
+library(jtools)
+
+
+rm(list = ls())
+mydal1=read.csv("C:/Users/bridg/OneDrive/Desktop/Econometric R Data/Alabama FRED HW.csv",header = TRUE)
+
+head(mydal1)
+
+mydal= subset(mydal1,YEAR>=1985)
+View(mydal)
+
+mydal$ALINC = mydal$ALINCOME/1000
+
+plot(x=mydal$ALINC, y=mydal$PCRIMER,xlab = "real household income",ylab = "AL property crime rate",main = "Scatter plot of Real Household Income and propert crime  rate for Alabama", pch=21, bg="blue")
+
+
+reg1 = lm(PCRIMER ~ ALUR, data=mydal)
+reg2 = lm(PCRIMER ~ ALUR + ALINC, data=mydal)
+reg3 = lm(PCRIMER ~ ALUR + ALINC + ALHOWN, data=mydal)
+summ(reg1, digits = getOption("jtools-digits",4))
+summ(reg2, digits = getOption("jtools-digits",4))
+summ(reg3, digits = getOption("jtools-digits",4))
+
+stargazer(mydal, type = 'text')
